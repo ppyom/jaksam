f8215fa6423afa218783f57ffb357153da486e4f
ppyom <ouob77@kakao.com>
2024-08-23 16:02:35 +0900

    feat: Post model method 구현 #1
    * ObjectId를 만드는 부분을 별도의 private method로 생성

--- models/post.js
+++ models/post.js
@@ -1,9 +1,34 @@
+import { ObjectId } from 'mongodb';
+
 class Post {
   static #database;
   static #postCollection;
   static injectDB(database) {
     this.#database = database;
     this.#postCollection = database.collection('posts');
+  }
+  static async getAll() {
+    return this.#postCollection.find().sort({ _id: -1 }).toArray();
+  }
+  static async getOne(id) {
+    return this.#postCollection.findOne({ _id: this.#makeObjectId(id) });
+  }
+  static async create(postData) {
+    return await this.#postCollection.insertOne(postData);
+  }
+  static async update(id, postData) {
+    return await this.#postCollection.updateOne(
+      { _id: this.#makeObjectId(id) },
+      { $set: postData },
+    );
+  }
+  static async delete(id) {
+    return await this.#postCollection.deleteOne({
+      _id: this.#makeObjectId(id),
+    });
+  }
+  static #makeObjectId(id) {
+    return new ObjectId(id);
   }
 }
 
